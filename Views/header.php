e330d84fdaf91acb60b84560761460b72f4c28ea
Ambaram  Srivastava <ambaramsrivastava@gmail.com>
2021-10-06 22:11:15 +0530

    session issue

--- Views/header.php
+++ Views/header.php
@@ -14,17 +14,17 @@
     <nav class="navbar justify-content-center m-0" id="nav">
         <div class="navbar navbar-sm-collapse text-monospace" id="fixnav">
             <div class="navbar-nav d-flex flex-row" id="links">
-                <a class="nav-item nav-link py-4 px-3 text-info" id="navbar-link" href="/Views/index.php#about">About us</a>
-                <a class="nav-item nav-link py-4 px-3 text-info" id="navbar-link" href="../Views/listtickets.php">View Tickets</a>
+                <a class="nav-item nav-link py-4 px-3 text-info" id="navbar-link" href="./index.php#about">About us</a>
+                <a class="nav-item nav-link py-4 px-3 text-info" id="navbar-link" href="./listtickets.php">View Tickets</a>
                 <a class="navbar-brand mx-4 py-0 text-center" href="./index.php">
                     <img src="../images/picture.png" class="rounded-circle w-50" alt="logo">
                 </a>
-                <a class="nav-item nav-link py-4 px-4 text-info" id="navbar-link" href="#contact">Contact</a>
+                <a class="nav-item nav-link py-4 px-4 text-info" id="navbar-link" href="./index.php#contact">Contact</a>
                 <?php
 if (isset($_SESSION['id'])) {?>
-                    <a class="nav-item nav-link py-4 px-3 text-info" id="navbar-link" href="../Views/login.php">Logout</a>
+                    <a class="nav-item nav-link py-4 px-3 text-info" id="navbar-link" href="./login.php">Logout</a>
                 <?php } else {?>
-                    <a class="nav-item nav-link py-4 px-3 text-info" id="navbar-link" href="../Views/logout.php">Login / Signup</a>
+                    <a class="nav-item nav-link py-4 px-3 text-info" id="navbar-link" href="./logout.php">Login / Signup</a>
                <?php }?>
             </div>
         </div>
